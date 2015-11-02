383920fd5e805efb2d959ae7eaf79a273c4ce8f1
OdieD8 <odie_de_moura@hotmail.com>
2015-11-02 14:10:38 -0700

    First commit

---
+++ js/products/productsCtrl.js
@@ -0,0 +1,8 @@
+angular.module("miniRouting").controller("productsCtrl", function($scope, $stateParams, productsService) {
+	if($stateParams.id === "shoes") {
+		$scope.products = productsService.shoeData;
+	}
+	if($stateParams.id === "socks") {
+		$scope.products = productsService.sockData;
+	}
+});
